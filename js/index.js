6748436b56af4565113ab18457a4ffa9a5054465
jfkinslow <killajk2201@gmail.com>
2019-04-10 23:10:40 -0500

    Fixed a few bugs in the script

--- js/index.js
+++ js/index.js
@@ -14,17 +14,31 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "top": [
+      {
+        "h4":"Features",
+        "content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+      },
+      {
+        "h4":"About",
+        "content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+      }
+    ],
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "bottom": [
+    {
+      "h4":"Services",
+      "content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    },
+    {
+      "h4":"Product",
+      "content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    },
+    {
+      "h4":"Vision",
+      "content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    }
+    ]
   },
   "contact": {
     "contact-h4" : "Contact",
@@ -47,8 +61,12 @@ navList.forEach((current) => {
   current.innerText = siteContent["nav"][`nav-item-${counter}`];
   counter++;
 });
-let newNavItem1 = document.createElement('a').setAttribute('href', "#").innerText = "First JS Child";
-let newNavItem2 = document.createElement('a').setAttribute('href', "#").innerText = "Second JS Child";
+let newNavItem1 = document.createElement('a')
+newNavItem1.setAttribute('href', "#")
+newNavItem1.innerText = "First JS Child";
+let newNavItem2 = document.createElement('a')
+newNavItem1.setAttribute('href', "#")
+newNavItem2.innerText = "Second JS Child";
 nav.prepend(newNavItem1);
 nav.appendChild(newNavItem2);
 let splitItem = siteContent["cta"]["h1"].split(" ");
@@ -57,18 +75,18 @@ document.querySelector('.container .cta .cta-text button').innerText = siteConte
 document.getElementById('cta-img').setAttribute('src', siteContent["cta"]["img-src"]);
 let topText = document.querySelectorAll('.container .main-content .top-content .text-content');
 console.log(topText);
-topText[0].querySelector("h4").innerHTML = `${siteContent["main-content"]["features-h4"]}`;
-topText[0].querySelector("p").innerHTML = `${siteContent["main-content"]["features-content"]}`;
-topText[1].querySelector("h4").innerHTML = `${siteContent["main-content"]["about-h4"]}`;
-topText[1].querySelector("p").innerHTML = `${siteContent["main-content"]["about-content"]}`;
+counter = 0;
+topText.forEach((current) => {
+  current.innerHTML = `<h4>${siteContent["main-content"]["top"][counter]["h4"]}</h4><p>${siteContent["main-content"]["top"][counter]["content"]}</p>`;
+  counter++
+});
 document.querySelector('.middle-img').setAttribute('src', siteContent["main-content"]["middle-img-src"]);
 let bottomText = document.querySelectorAll('.container .main-content .bottom-content .text-content');
-bottomText[0].querySelector("h4").innerHTML = `${siteContent["main-content"]["services-h4"]}`;
-bottomText[0].querySelector("p").innerHTML = `${siteContent["main-content"]["services-content"]}`;
-bottomText[1].querySelector("h4").innerHTML = `${siteContent["main-content"]["product-h4"]}`;
-bottomText[1].querySelector("p").innerHTML = `${siteContent["main-content"]["product-content"]}`;
-bottomText[2].querySelector("h4").innerHTML = `${siteContent["main-content"]["vision-h4"]}`;
-bottomText[2].querySelector("p").innerHTML = `${siteContent["main-content"]["vision-content"]}`;
+counter = 0
+bottomText.forEach((current) => {
+  current.innerHTML = `<h4>${siteContent["main-content"]["bottom"][counter]["h4"]}</h4><p>${siteContent["main-content"]["bottom"][counter]["content"]}`;
+  counter ++
+})
 let addrSplit = siteContent["contact"]["address"].split(" ");
 let contactHTML = `<h4>${siteContent["contact"]["contact-h4"]}</h4><p>${addrSplit[0]} ${addrSplit[1]} ${addrSplit[2]} ${addrSplit[3]}<br>${addrSplit[4]} ${addrSplit[5]}</p><p>${siteContent["contact"]["phone"]}</p><p>${siteContent["contact"]["email"]}</p>`;
 document.querySelector('.container .contact').innerHTML = contactHTML;
